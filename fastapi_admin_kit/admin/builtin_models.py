48f6e1d5b5106f2ed910c0a1cc5f5b5e5fc38e01
borhanst <mdborhan.st@gmail.com>
2026-07-13 00:44:06 +0600

    fix: role table register admin

--- fastapi_admin_kit/admin/builtin_models.py
+++ fastapi_admin_kit/admin/builtin_models.py
@@ -206,7 +206,7 @@ class RoleAdmin(ModelAdmin):
     icon = "shield-check"
     list_display = ["id", "name", "description"]
     search_fields = ["name"]
-    skip_auto_routes = True
+    # skip_auto_routes = True
     exclude = ["users"]
 
 
